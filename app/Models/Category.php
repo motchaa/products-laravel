57e02202dfe51af11fec59d960c077e008c92934
Mota <thiago.motahonda11@gmail.com>
2026-07-14 09:54:40 -0300

    feat(models): add Product and Category models with soft deletes

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Category extends Model
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    protected $fillable = [
+        'nome',
+        'descricao',
+        'ativo'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'categoria_id');
+    }
+
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Product extends Model
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    protected $fillable = [
+        'nome',
+        'descricao',
+        'codigo',
+        'valor',
+        'quantidade',
+        'ativo',
+        'categoria_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'categoria_id');
+    }
+}
